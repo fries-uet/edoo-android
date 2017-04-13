e30b55c3bb868c4d425569cf36e585c9b1ace4ca
quytm <minhquylt95@gmail.com>
2017-04-14 00:19:19 +0700

    Login Screen

---
+++ app/src/main/java/com/uet/fries/tmq/edoo/activity/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.uet.fries.tmq.edoo.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import com.uet.fries.tmq.edoo.R;
+
+
+public class LoginActivity extends Activity {
+    private static final String TAG = LoginActivity.class.getSimpleName();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_login);
+
+    }
+}
--- app/src/main/java/com/uet/fries/tmq/edoo/activity/SplashScreenActivity.java
+++ app/src/main/java/com/uet/fries/tmq/edoo/activity/SplashScreenActivity.java
@@ -18,5 +18,13 @@ public class SplashScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1000);
     }
 }
