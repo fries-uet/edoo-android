d7c82e1a32790dc3c3b4a8f004c2a6a7c8b0829b
quytm <minhquylt95@gmail.com>
2017-04-14 00:14:52 +0700

    layout SplashScreen

---
+++ app/src/main/java/com/uet/fries/tmq/edoo/activity/SplashScreenActivity.java
@@ -0,0 +1,22 @@
+package com.uet.fries.tmq.edoo.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import com.uet.fries.tmq.edoo.R;
+
+
+/**
+ * Created by tmq on 08/09/2016.
+ */
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+    }
+}
---
+++ app/src/main/java/com/uet/fries/tmq/edoo/app/AppController.java
@@ -0,0 +1,21 @@
+package com.uet.fries.tmq.edoo.app;
+
+import android.support.multidex.MultiDexApplication;
+
+public class AppController extends MultiDexApplication {
+	public static final String TAG = AppController.class.getSimpleName();
+
+	private static AppController mInstance;
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		mInstance = this;
+	}
+
+	public static synchronized AppController getInstance() {
+		return mInstance;
+	}
+
+}
